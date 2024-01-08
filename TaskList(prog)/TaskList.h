c5ebdfb8b49d525c8daa86a2975a0831b2405283
Nikita <nik.dmitrienko2000@gmail.com>
2024-01-08 12:55:19 +0200

    added some methods

--- TaskList(prog)/TaskList.h
+++ TaskList(prog)/TaskList.h
@@ -1,10 +1,17 @@
 #pragma once
-
+/*—оздайте класс TaskList, представл€ющий список задач.
+¬ключите методы дл€ добавлени€, удалени€ и отображени€ задач в списке.
+–еализуйте методы дл€ сортировки задач по различным критери€м (например, по статусу или сроку выполнени€).*/
 class TaskList
 {
 private:
 
 public:
+	void AddTask();
+	void DeleteTask();
+	void ShowTask() const;
+	void TaskStatusSorting();
+	void TaskDeadlineSorting();
 
 };
 
