a415c4789532a36d3f797d5a13363b7463ab8188
Nikita <nik.dmitrienko2000@gmail.com>
2024-01-08 13:00:11 +0200

    Create methods in .cpp

--- TaskList(prog)/TaskList.cpp
+++ TaskList(prog)/TaskList.cpp
@@ -1,1 +1,21 @@
 #include "TaskList.h"
+
+void TaskList::AddTask()
+{
+}
+
+void TaskList::DeleteTask()
+{
+}
+
+void TaskList::ShowTask() const
+{
+}
+
+void TaskList::TaskStatusSorting()
+{
+}
+
+void TaskList::TaskDeadlineSorting()
+{
+}
